e36b86e9c18fa6625050f5f8a96c471a7f3fd35c
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-03 00:43:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+let firstName=document.getElementByName("fname");
+let lastName=document.getElementByName("lname");
+alert(firstName+lastName)
 
 }
